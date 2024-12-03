b3128975215eaa79b9a15941d34a21162c3b2384
Advait Parab <advait0012@proton.me>
2024-12-03 08:01:46 +0000

    ui done

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
 import Header from "@/components/Header";
+import Preview from "@/components/Preview";
+import Configurator from "@/components/Configurator";
 
 function HomePage() {
   return (
     <>
       <Header />
+      <main className="flex flex-col sm:flex-row justify-between md:gap-4">
+        <section className="w-full md:w-3/4 md:gap-8 px-2 md:px-8 py-3 md:py-6">
+          <Preview />
+        </section>
+        <aside className="w-full md:w-1/4">
+        <Configurator />
+        </aside>
+      </main>
     </>
   );
 }
